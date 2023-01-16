106bd54828f5370e366d0393e3dc31dd4a09e6ba
feliciaa6a <1638260165@qq.com>
2023-01-16 11:05:04 +0800

    third commit

---
+++ load/load.js
@@ -0,0 +1,30 @@
+let submit=document.getElementById("submit");
+let usernameInput=document.getElementById("username");
+let passwordInput=document.getElementById("password");
+submit.onclick=function(){ 
+    ///获取用户名和密码
+    let username=usernameInput.value;
+    let password=passwordInput.value;
+    fetch("https://api.github.com/search/users?q=runninglili",
+    {method:'post',
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:{"name":username,"password":password}
+    })
+    .then((response)=>{
+        console.log('response',response);
+        if(response.status===200){
+            return response.json();
+        }
+    })
+    .then((json)=>{
+        if (json.code == "200") {
+            console.log("success");
+            location.href='./home.html'
+            }else if (json.code == "400") {         
+            console.log("failed")            
+            }
+    })
+    .catch(err=>console.log('request failed',err));
+}
